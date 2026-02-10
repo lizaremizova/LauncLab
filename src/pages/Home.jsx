63ad844334ba51c545a8439dff62704734af96c4
23DP2ERemi <A231498ER@rvt.lv>
2026-02-10 13:21:57 +0200

    feat: added slider section to homepage

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,26 @@
 import Header from "../components/Header/Header";
 import styles from "./Home.module.css";
 import Hero from "../components/Hero/Hero";
+import Carousel from "../components/Slider/Carousel";
 
 export default function Home() {
     return (
         <div className={styles.home}>
             <Header />
             <Hero />
+            <section className={styles.sliderBg}>
+                <div style={{ position: "relative"}}>
+                    <Carousel
+                        baseWidth={800}
+                        autoplay={true}
+                        autoplayDelay={3000}
+                        pauseOnHover={true}
+                        loop={true}
+                        round={false}
+                    />
+                </div>
+            </section>
+
         </div>
     );
 }
